e226f42a62fcdc3395e248ce9cb805b11a81a54f
haiphi <ahodges230@gmail.com>
2017-07-31 17:49:04 -0500

    Added counter and removed extra divs

--- client.js
+++ client.js
@@ -1,39 +1,24 @@
-// console.log("JS");
-var count = 0;
- $('p').on('click', function(){
-    
- });
+
 
 $(document).ready(function(){
+   var counter = 0;
   $('button').on('click',function(){
-     $('.container').append('<div></div>');
-      $('.container').append('<p class="count">' + '</p>');
-      $('.count').on('click', function(){
+    counter++;
+    //Appends div, p and buttons to div. Also increments count with each div
+     $('.container').append('<div>' + '<p>' + counter + '</p>' +
+     '<button class="swap">Swap</button>' +
+        '<button class="delete">Delete</button>'  + '</div>');
 
-      });
-      //Append button with the class swap the the div
-      $('div').append('<button class="swap">Swap</button>');
-
-      //Append delete button with the class swap the the div
-      $('div').append('<button class="delete">Delete</button>');
-
-      //When button is clicked, delete button
+      //When button is clicked, delete div
       $('.delete').on('click', function(){
         $(this).closest('div').remove();
 
-        //Change div color from red to yellow using toggle class
+        //Change div color from red to yellow using toggleClass
         $(".swap").click(function(){
           $(this).toggleClass("yellow");
-        //  $('.swap').on('click', function(){
-        //    $(this).toggle("yellow");
-         });
-        // $('button').on('click', '.swap',function(){
-        //   $(this).toggleClass("yellow");
-        });
-      });
 
+          }); //swap listener
 
-
-  //  });
-
- });
+        });//delete listener
+      }); //button
+ }); //doc ready
