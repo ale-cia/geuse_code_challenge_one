79c6aac712d2e96104462ab5e4774cdac9aadb47
ale-cia <ahodges230@gmail.com>
2017-07-31 22:29:08 -0500

    Attempting color swap

--- client.js
+++ client.js
@@ -14,8 +14,15 @@ $(document).ready(function(){
         $(this).closest('div').remove();
 
         //Change div color from red to yellow using toggleClass
-        $(".swap").click(function(){
+        $(".swap").on('click', function(){
           $(this).toggleClass("yellow");
+
+        //   $(this).toggleClass("yellow");
+        //   $(this).parent().toggleClass("yellow");
+        //   $('button').parent().toggleClass("yellow");
+        //   $('button').on('click', '.swap', function(){
+        //   $(this).toggleClass("yellow");
+        //   $('.container').toggleClass("yellow");
 
           }); //swap listener
 
